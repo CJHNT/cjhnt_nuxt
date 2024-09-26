b4465c50c145b121a9b6a3d87e5bf7e47f41046b
Matthew Munson <munson@saw-leipzig.de>
2024-09-26 14:37:05 +0200

    Added page loading spinner

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,11 +4,23 @@ import './assets/main.css'
 // function toggleTheme() {
 //   theme.value = theme.value === 'light' ? 'dark' : 'light'
 // }
+const nuxtApp = useNuxtApp()
+const loading = ref(false)
+nuxtApp.hook('page:start', () => {
+  loading.value = true
+})
+
+nuxtApp.hook('page:finish', () => {
+  loading.value = false
+})
 </script>
 
 <template>
   <v-app class="rounded rounded-md">
     <AppBar />
+    <v-overlay v-model="loading" class="align-center justify-center">
+      <v-progress-circular color="primary" size="64" indeterminate></v-progress-circular>
+    </v-overlay>
     <NotificationContainer />
     <slot />
   </v-app>
