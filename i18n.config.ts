eaefc29bbfb6c3a9d4be4a0dcab5087a0a9ec0d5
Matthew Munson <munson@saw-leipzig.de>
2024-11-05 11:32:16 +0100

    Added Vuetify I18n messages

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,11 +1,12 @@
 import enLang from './lang/en.json'
 import deLang from './lang/de.json'
+import { en, de } from 'vuetify/locale'
 
 export default defineI18nConfig(() => ({
   legacy: false,
   locale: 'en',
   messages: {
-    en: enLang,
-    de: deLang
+    en: { ...enLang, $vuetify: en },
+    de: { ...deLang, $vuetify: de }
   }
 }))
