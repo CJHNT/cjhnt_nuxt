05f83d0f97629c73f81644883eb56330dcf5401d
Matthew Munson <munson@saw-leipzig.de>
2024-11-05 11:42:50 +0100

    Added exports to verify users by email and by ID (for password reset)
    Also removed unused exports

--- utils/db.ts
+++ utils/db.ts
@@ -94,29 +94,29 @@ export const verifyPassword = async (email: string, password: string) => {
   return false
 }
 
+export const verifyUserByEmail = async (email: string) => {
+  const result = db
+    .prepare<[string], { id: string }>('SELECT id FROM users WHERE email=?')
+    .get(email)
+  if (result !== undefined) {
+    return result.id
+  }
+  return false
+}
+
+export const verifyUserById = async (userId: string) => {
+  const result = db.prepare<[string], { id: number }>('SELECT id FROM users WHERE id=?').get(userId)
+  if (result !== undefined) {
+    return result.id
+  }
+  return false
+}
+
 export const verifySession = (userId: string, sessionToken: string) => {
   const result = db
     .prepare<[string, string], { id: string }>('SELECT id FROM users WHERE id=? AND sessionToken=?')
     .get(userId, sessionToken)
   return result !== undefined && result.id === userId
-}
-
-export const getPostsByUser = (userId: string) => {
-  const result = db
-    .prepare<
-      [string],
-      { id: string; content: string }
-    >(`SELECT id, content FROM posts WHERE userId=?`)
-    .all(userId)
-  return Array.isArray(result) ? result : []
-}
-
-export const createPost = (userId: string, content: string) => {
-  const id = cryptoRandomString({ length: 21, type: 'base64' })
-  const info = db
-    .prepare('INSERT INTO posts (id, content, userId) VALUES (?, ?, ?)')
-    .run(id, content, userId)
-  return info.changes === 1
 }
 
 export const getCsrfToken = (userId: string) => {
