64191ca3ce637b4c5140db3db0d87db5493f1cec
Matthew Munson <munson@saw-leipzig.de>
2024-11-06 17:00:05 +0100

    Eng and Deu versions of the password reset email

--- server/api/auth/forgotPassword.ts
+++ server/api/auth/forgotPassword.ts
@@ -5,6 +5,7 @@ export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig()
   const body = await readBody(event)
   const userExists = await verifyUserByEmail(body.payload.email)
+  const t = await useTranslation(event)
   if (userExists) {
     const token = jwt.sign({ userId: userExists }, config.sessionPassword, {
       expiresIn: 600
@@ -14,12 +15,35 @@ export default defineEventHandler(async (event) => {
       body: {
         mailRecipient: body.payload.email,
         mailSubject: 'Password Reset for CJHNT-digital',
-        mailText:
-          'We are trying to reset your password ' +
-          `${config.baseUrl}auth/resetPassword?token=${token}`,
-        mailHtml:
-          'We are <i>trying</i> to reset your password ' +
-          `${config.baseUrl}auth/resetPassword?token=${token}`
+        mailText: `${t('auth.passwordResetEmail.greeting')}  ${body.payload.email},
+
+          ${t('auth.passwordResetEmail.htmlReset')} ${t('auth.passwordResetEmail.clickHere')}
+
+          ${config.baseUrl}auth/resetPassword?token=${token}
+
+          ${t('auth.passwordResetEmail.ignore')}
+
+          ${t('auth.passwordResetEmail.sincerely')}
+          
+          ${t('auth.passwordResetEmail.sender')}
+          `,
+        mailHtml: `<!doctype html>
+          <html>
+            <body>
+              <p>${t('auth.passwordResetEmail.greeting', [body.payload.email])}</p>
+              <p>
+                  ${t('auth.passwordResetEmail.htmlReset')}
+                  <a href="${config.baseUrl}auth/resetPassword?token=${token}">
+                      ${t('auth.passwordResetEmail.clickHere')}
+                  </a>.
+              </p>
+              <p>${t('auth.passwordResetEmail.alternatively')}</p>
+              <p>${config.baseUrl}auth/resetPassword?token=${token}</p>
+              <p>${t('auth.passwordResetEmail.ignore')}</p>
+              <p>${t('auth.passwordResetEmail.sincerely')}</p>
+              <p>${t('auth.passwordResetEmail.sender')}</p>
+            </body>
+          </html>`
       }
     })
   }
