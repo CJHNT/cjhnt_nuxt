792d8a82a6eea8366b33916577618b870b5eae79
Matthew Munson <munson@saw-leipzig.de>
2024-11-18 17:06:08 +0100

    I can now find and test the news cards on the home page

--- tests/e2e/index.test.ts
+++ tests/e2e/index.test.ts
@@ -1,31 +1,25 @@
-// import { setup, createPage } from '@nuxt/test-utils/e2e'
+import { setup, createPage } from '@nuxt/test-utils/e2e'
 
-// describe('index page', async () => {
-//   await setup()
+describe('index page', async () => {
+  await setup()
 
-//   test('smoke test', () => {
-//     expect(true).toBeTruthy()
-//   })
-//   test('Index page displays', async () => {
-//     const page = await createPage('/')
-//     const locater = page.locator('.news-title')
-//     const expectedTitles = [
-//       'Press release on the project launch',
-//       'Work Conference 2025',
-//       'The First Six Months...'
-//     ]
-//     expect(await locater.count()).toBe(3)
-//     // expect(page.locator('.news-title')).toHaveText(['Press release on the project launch', 'Work Conference 2025', 'The First Six Months...'])
-//   })
-// })
-import { expect, test } from '@nuxt/test-utils/playwright'
-
-test('index page displays', async ({ page, goto }) => {
-  await goto('/', { waitUntil: 'hydration' })
-  const locator = page.locator('.news-title')
-  expect(locator).toHaveText([
-    'Press release on the project launch',
-    'Work Conference 2025',
-    'The First Six Months...'
-  ])
+  test('smoke test', () => {
+    expect(true).toBeTruthy()
+  })
+  test('Index page displays', async () => {
+    const page = await createPage('/')
+    const locater = page.locator('.news-title')
+    const expectedTitles = [
+      'Press release on the project launch',
+      'Work Conference 2025',
+      'The First Six Months...'
+    ]
+    expect(await locater.count()).toBe(3)
+    expect(await locater.allInnerTexts()).toStrictEqual([
+      'Press release on the project launch',
+      'Work Conference 2025',
+      'The First Six Months...'
+    ])
+    // expect(page.locator('.news-title')).toHaveText(['Press release on the project launch', 'Work Conference 2025', 'The First Six Months...'])
+  })
 })
