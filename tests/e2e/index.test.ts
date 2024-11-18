fcb59f4fc2d198bfad64b16fd0bfa245e58ce4b9
Matthew Munson <munson@saw-leipzig.de>
2024-11-18 16:57:34 +0100

    Working on E2E tests

--- tests/e2e/index.test.ts
+++ tests/e2e/index.test.ts
@@ -1,13 +1,31 @@
-import { mount, flushPromises } from '@vue/test-utils'
-import App from '../../app.vue'
+// import { setup, createPage } from '@nuxt/test-utils/e2e'
 
-describe('Index page', async () => {
-  test('smoke test', () => {
-    expect(true).toBeTruthy()
-  })
-  test('that HTML exists', async () => {
-    const wrapper = mount(App)
-    await flushPromises()
-    expect(wrapper.text()).toContain('Welcome')
-  })
+// describe('index page', async () => {
+//   await setup()
+
+//   test('smoke test', () => {
+//     expect(true).toBeTruthy()
+//   })
+//   test('Index page displays', async () => {
+//     const page = await createPage('/')
+//     const locater = page.locator('.news-title')
+//     const expectedTitles = [
+//       'Press release on the project launch',
+//       'Work Conference 2025',
+//       'The First Six Months...'
+//     ]
+//     expect(await locater.count()).toBe(3)
+//     // expect(page.locator('.news-title')).toHaveText(['Press release on the project launch', 'Work Conference 2025', 'The First Six Months...'])
+//   })
+// })
+import { expect, test } from '@nuxt/test-utils/playwright'
+
+test('index page displays', async ({ page, goto }) => {
+  await goto('/', { waitUntil: 'hydration' })
+  const locator = page.locator('.news-title')
+  expect(locator).toHaveText([
+    'Press release on the project launch',
+    'Work Conference 2025',
+    'The First Six Months...'
+  ])
 })
